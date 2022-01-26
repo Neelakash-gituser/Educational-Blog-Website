3b4347db507931b94adc30eeb34c60d47b7695ae
Neelakash-gituser <gamingdeity98@gmail.com>
2022-01-26 17:45:12 +0530

    New Changes

--- csandphysicsblog/forms.py
+++ csandphysicsblog/forms.py
@@ -1,7 +1,7 @@
 from csandphysicsblog.models import Connect
 from django import forms
 
-class Contact(forms.ModelForm): # use forms.ModelField when using a model as form else use forms.Form for just a normal form .
+class Contact(forms.ModelForm): # use forms.ModelForm when using a model as form else use forms.Form for just a normal form .
     class Meta:
         model = Connect  
         fields = "__all__"
